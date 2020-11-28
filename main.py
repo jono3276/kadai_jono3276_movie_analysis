8c218aface98991d9310bb9c342cf5d86eb90925
jonokai2276 <jonokai2276@gmail.com>
2020-11-29 00:32:32 +0900

    課題

--- main.py
+++ main.py
@@ -14,11 +14,37 @@ def initialize_database() -> None:
 
     conn.close()
 
+def fetch_income_genre() -> list:
+    conn = sqlite3.connect('my_movies.db')
+    sql = 'SELECT sum(income) AS sum_income, genre FROM my_movies GROUP BY genre ORDER BY sum_income DESC ;'
+
+    return conn.execute(sql).fetchall()
+
+
+def sort_genre_to_income(results_income_genre) -> list:
+    genre_income_list = []
+    for result in results_income_genre:
+        sum_income, genre = map(str, result)
+        genre_income_list.append(f'{genre},{sum_income}')
+    return genre_income_list
+
+
+def make_csv_file(genre_income_list):
+    with open('output.csv', mode='w', encoding='utf-8') as f:
+        output = '\n'.join(genre_income_list)
+        f.write(output)
+
+
 
 def main():
     initialize_database()
 
     # 以下から書き始めること！
+    income_genre_list = fetch_income_genre()
+
+    genre_income_list = sort_genre_to_income(income_genre_list)
+
+    make_csv_file(genre_income_list)
 
 
 if __name__ == '__main__':
